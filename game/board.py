943c2d97d282166d6650e2f5c15b075676d751f7
SavagePastaMan <69145546+SavagePastaMan@users.noreply.github.com>
2021-11-10 12:57:37 -0500

    Implement `_check_win`.
    Also added tests for various win-conditions.
    
    Added scipy as a dependency.

--- game/board.py
+++ game/board.py
@@ -1,4 +1,10 @@
 import numpy as np
+from scipy.signal import convolve
+
+from typing import Optional
+
+
+KERNEL = np.ones((4, 4), dtype=np.byte)
 
 
 class Board:
@@ -6,24 +12,27 @@ class Board:
     Maintains the board state of the game.
 
     Attributes:
-        grid: A numpy array containing the board state
         turn: An int, either -1 or 1, that keeps track of whose turn it is
     """
 
     def __init__(self):
-        self.grid = np.zeros((6, 7), dtype=np.byte)
+        self._grid = np.zeros((6, 7), dtype=np.byte)
 
         self.turn = 1
 
-    def add_move(self, col: int) -> bool:
+    def add_move(self, col: int) -> Optional[int]:
         """
         Given a column, try to insert a piece at that column.
+
+        If adding the piece does not result in a win, update the `turn` attribute
+        to switch turns.
 
         Args:
             col: The column to try to insert a piece at
 
         Returns:
-            True if by inserting the piece, the player has won. False otherwise
+            If a player has won, return `self.turn`
+            Otherwise, return None
 
         Raises:
             ValueError: The column was out of bounds, or the column was full.
@@ -32,28 +41,27 @@ class Board:
         if col < 0 or col > 6:
             raise ValueError(f"Column must be in the range 0 <= col < 7")
 
-        row = len(self.grid[:, col].nonzero()[0])
+        row = len(self._grid[:, col].nonzero()[0])
 
         # TODO: Check if the column is full
 
-        self.grid[row, col] = self.turn
+        self._grid[row, col] = self.turn
 
-        # TODO: Check if the player has won after inserting this piece
+        if self._check_win():
+            return self.turn
 
         self.turn *= -1
 
-        return False
+        return None
 
-    def _check_win(self, row: int, col: int) -> bool:
+    def _check_win(self) -> bool:
         """
         Check if there is a 4-in-a-row that includes the point (row, col).
-
-        Args:
-            row: The row of the point
-            col: The column of the point
 
         Returns:
             True if there is a 4-in-a-row, false otherwise.
         """
 
-        pass
+        neighbors = convolve(self._grid, KERNEL)
+
+        return abs(neighbors).max() >= 4
--- tests/test_board.py
+++ tests/test_board.py
@@ -23,7 +23,7 @@ def test_board_add():
     b = board.Board()
     b.add_move(3)
 
-    assert b.grid[0, 3] == 1
+    assert b._grid[0, 3] == 1
 
 
 def test_switch_turn():
@@ -31,3 +31,61 @@ def test_switch_turn():
     b.add_move(3)
 
     assert b.turn == -1
+
+
+def test_win_vertical():
+    # Board state to test
+    # x
+    # x o
+    # x o
+    # x o
+
+    b = board.Board()
+    for _ in range(3):
+        b.add_move(0)
+        b.add_move(1)
+
+    b.add_move(0)
+
+    print(b._grid)
+
+    assert b._check_win()
+
+
+def test_win_horizontal():
+    # Board state to test
+    # o o o
+    # x x x x
+
+    b = board.Board()
+    for i in range(3):
+        b.add_move(i)
+        b.add_move(i)
+
+    b.add_move(3)
+    print(b._grid)
+
+    assert b._check_win()
+
+
+def test_win_diagonal():
+    # Board state to test
+    #       x
+    #     x o
+    # o x x x
+    # x o o o
+
+    b = board.Board()
+    b.add_move(0)
+    b.add_move(1)
+    b.add_move(1)
+    b.add_move(2)
+    b.add_move(2)
+    b.add_move(3)
+    b.add_move(2)
+    b.add_move(0)
+    b.add_move(3)
+    b.add_move(3)
+    b.add_move(3)
+
+    assert b._check_win()
