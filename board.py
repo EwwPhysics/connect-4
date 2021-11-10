fab12cbac6b0233178997bde8e8e607514e7cc83
SavagePastaMan <69145546+savagepastaman@users.noreply.github.com>
2021-11-10 08:57:27 -0500

    Create `Board` class to maintain game state.
    Partially implemented `add_move` to handle adding a piece to a column. Still need to check if the player has won.
    Return type could change in future, might return the player who won instead of simply true/false.
    
    Added docstring and function signature for `_check_win`, helper method to determine if a player has won.

---
+++ board.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+class Board:
+    """
+    Maintains the board state of the game.
+
+    Attributes:
+        grid: A numpy array containing the board state
+        turn: An int, either -1 or 1, that keeps track of whose turn it is
+    """
+
+    def __init__(self):
+        self.grid = np.zeros((6, 7), dtype=np.byte)
+
+        self.turn = 1
+
+    def add_move(self, col: int) -> bool:
+        """
+        Given a column, try to insert a piece at that column.
+
+        Args:
+            col: The column to try to insert a piece at
+
+        Returns:
+            True if by inserting the piece, the player has won. False otherwise
+
+        Raises:
+            ValueError: The column was out of bounds, or the column was full.
+        """
+
+        if col < 0 or col > 6:
+            raise ValueError(f"Column must be in the range 0 <= col < 7")
+
+        t = self.grid[:, col].nonzero()[0]
+        self.grid[len(t), col] = self.turn
+
+        # TODO: Check if the player has won after inserting this piece
+
+        self.turn *= -1
+
+        return False
+
+    def _check_win(self, row: int, col: int) -> bool:
+        """
+        Check if there is a 4-in-a-row that includes the point (row, col).
+
+        Args:
+            row: The row of the point
+            col: The column of the point
+
+        Returns:
+            True if there is a 4-in-a-row, false otherwise.
+        """
+
+        pass
